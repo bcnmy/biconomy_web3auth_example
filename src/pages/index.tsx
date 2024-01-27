6311efe4302ea4ee7b4f82c3017357d82f93a8ae
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2024-01-27 23:43:40 +0530

    Integration Done

---
+++ src/pages/index.tsx
@@ -0,0 +1,238 @@
+import React, { useState } from "react";
+import {
+  BiconomySmartAccountV2,
+  BiconomySmartAccountV2Config,
+} from "@biconomy/account";
+import {
+  IHybridPaymaster,
+  SponsorUserOperationDto,
+  PaymasterMode,
+} from "@biconomy/paymaster";
+import { ethers } from "ethers";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import { contractABI } from "../contract/contractABI";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Create a provider for the Polygon Mumbai network
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc.ankr.com/polygon_mumbai"
+);
+
+// Specify the chain ID for Polygon Mumbai
+let chainId = 80001; // Polygon Mumbai or change as per your preferred chain
+
+export default function Home() {
+  const [smartAccount, setSmartAccount] =
+    useState<BiconomySmartAccountV2 | null>(null);
+  const [smartAccountAddress, setSmartAccountAddress] = useState<string | null>(
+    null
+  );
+  const [count, setCount] = useState<string | null>(null);
+  const [txnHash, setTxnHash] = useState<string | null>(null);
+
+  const connect = async () => {
+    try {
+      const chainConfig = {
+        chainNamespace: CHAIN_NAMESPACES.EIP155,
+        chainId: "0x13881",
+        rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
+        displayName: "Polygon Mumbai",
+        blockExplorer: "https://mumbai.polygonscan.com/",
+        ticker: "MATIC",
+        tickerName: "Polygon Matic",
+      };
+
+      //Creating web3auth instance
+      const web3auth = new Web3Auth({
+        clientId:
+          "BExrkk4gXp86e9VCrpxpjQYvmojRSKHstPRczQA10UQM94S5FtsZcxx4Cg5zk58F7W1cAGNVx1-NPJCTFIzqdbs", // Get your Client ID from the Web3Auth Dashboard https://dashboard.web3auth.io/
+        web3AuthNetwork: "sapphire_devnet", // Web3Auth Network
+        chainConfig,
+        uiConfig: {
+          appName: "Biconomy X Web3Auth",
+          mode: "dark", // light, dark or auto
+          loginMethodsOrder: ["apple", "google", "twitter"],
+          logoLight: "https://web3auth.io/images/web3auth-logo.svg",
+          logoDark: "https://web3auth.io/images/web3auth-logo---Dark.svg",
+          defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+          loginGridCol: 3,
+          primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+        },
+      });
+
+      await web3auth.initModal();
+      const web3authProvider = await web3auth.connect();
+      const ethersProvider = new ethers.providers.Web3Provider(
+        web3authProvider as any
+      );
+
+      const biconomySmartAccountConfig: BiconomySmartAccountV2Config = {
+        signer: ethersProvider.getSigner(),
+        chainId: chainId,
+        biconomyPaymasterApiKey:
+          "-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
+        bundlerUrl: `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
+      };
+
+      let biconomySmartAccount = await BiconomySmartAccountV2.create(
+        biconomySmartAccountConfig
+      );
+
+      console.log(biconomySmartAccount);
+      setSmartAccount(biconomySmartAccount);
+      const address = await biconomySmartAccount.getAccountAddress();
+      console.log(address);
+      setSmartAccountAddress(address);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCountId = async () => {
+    const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    const countId = await contractInstance.getCount();
+    setCount(countId.toString());
+  };
+
+  const incrementCount = async () => {
+    try {
+      const toastId = toast("Populating Transaction", { autoClose: false });
+
+      const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        provider
+      );
+      const minTx = await contractInstance.populateTransaction.increment();
+      console.log("Mint Tx Data", minTx.data);
+      const tx1 = {
+        to: contractAddress,
+        data: minTx.data,
+      };
+
+      toast.update(toastId, { render: "Building UserOp", autoClose: false });
+      let userOp = await smartAccount?.buildUserOp([tx1]);
+      console.log("UserOp", { userOp });
+      const biconomyPaymaster =
+        smartAccount?.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
+      let paymasterServiceData: SponsorUserOperationDto = {
+        mode: PaymasterMode.SPONSORED,
+        smartAccountInfo: {
+          name: "BICONOMY",
+          version: "2.0.0",
+        },
+      };
+      const paymasterAndDataResponse =
+        await biconomyPaymaster?.getPaymasterAndData(
+          //@ts-ignore
+          userOp,
+          paymasterServiceData
+        );
+
+      //@ts-ignore
+      userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
+
+      //Add the below if statement if you encounter AA34 signature Error
+      if (
+        userOp &&
+        paymasterAndDataResponse.callGasLimit &&
+        paymasterAndDataResponse.verificationGasLimit &&
+        paymasterAndDataResponse.preVerificationGas
+      ) {
+        // Returned gas limits must be replaced in your op as you update paymasterAndData.
+        // Because these are the limits paymaster service signed on to generate paymasterAndData
+        // If you receive AA34 error check here..
+
+        userOp.callGasLimit = paymasterAndDataResponse.callGasLimit;
+        userOp.verificationGasLimit =
+          paymasterAndDataResponse.verificationGasLimit;
+        userOp.preVerificationGas = paymasterAndDataResponse.preVerificationGas;
+      }
+
+      toast.update(toastId, { render: "Sending UserOp", autoClose: false });
+      //@ts-ignore
+      const userOpResponse = await smartAccount?.sendUserOp(userOp);
+      console.log("userOpHash", { userOpResponse });
+      //@ts-ignore
+      const { receipt } = await userOpResponse.wait(1);
+      console.log("txHash", receipt.transactionHash);
+
+      if (receipt.transactionHash) {
+        toast.update(toastId, {
+          render: "Transaction Successful",
+          type: "success",
+          autoClose: 5000,
+        });
+        setTxnHash(receipt.transactionHash);
+      }
+
+      await getCountId();
+    } catch (error) {
+      console.log(error);
+      toast.error("Transaction Unsuccessful", { autoClose: 5000 });
+    }
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
+      <div className="text-[4rem] font-bold text-orange-400">
+        Biconomy-Web3Auth
+      </div>
+      {!smartAccount && (
+        <button
+          className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+          onClick={connect}
+        >
+          Web3Auth Sign in
+        </button>
+      )}
+
+      {smartAccount && (
+        <>
+          {" "}
+          <span>Smart Account Address</span>
+          <span>{smartAccountAddress}</span>
+          <span>Network: Polygon Mumbai</span>
+          <div className="flex flex-row justify-between items-start gap-8">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <button
+                className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+                onClick={getCountId}
+              >
+                Get Count Id
+              </button>
+              <span>{count}</span>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-4">
+              <button
+                className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+                onClick={incrementCount}
+              >
+                Increment Count
+              </button>
+              {txnHash && (
+                <a
+                  target="_blank"
+                  href={`https://mumbai.polygonscan.com/tx/${txnHash}`}
+                >
+                  <span className="text-white font-bold underline">
+                    Txn Hash
+                  </span>
+                </a>
+              )}
+            </div>
+          </div>
+          <span className="text-white">Open console to view console logs.</span>
+        </>
+      )}
+    </main>
+  );
+}
